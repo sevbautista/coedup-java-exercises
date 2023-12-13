f8e3f1b36fe43cede76749cc1906d5f852fe2f94
Severiano Bautista <severiano.bautista090@gmail.com>
2023-12-13 16:50:33 -0600

    methods exercise

---
+++ src/MethodsExercises.java
@@ -0,0 +1,59 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+//        System.out.println(addition(5, 4));
+//        System.out.println(subtraction(5, 4));
+//        System.out.println(multiplication(5, 4));
+//        System.out.println(division(5, 4));
+//        System.out.println(modulus(15, 5));
+//        System.out.println(byZero(1,0));
+        System.out.println(multiSansStar(5, 3));
+    }
+
+//    public static int addition(int a, int b) {
+//        return a + b;
+//
+//    }
+//
+//    public static int subtraction(int a, int b) {
+//        return a - b;
+//
+//    }
+//
+//    public static int multiplication(int a, int b) {
+//        return a * b;
+//
+//    }
+//
+//    public static int division(int a, int b) {
+//        return a / b;
+//
+//    }
+//
+//    public static int modulus(int a, int b) {
+//        return a % b;
+//
+//    }
+
+    //        public static int byZero (int a, int b) { // SYSTEM DOES NOT LIKE THIS
+//        return a / b; // SHOULD RETURN ZERO
+//
+//    }
+//     5 * 4
+    public static int multiSansStar(int a, int b) {
+//    int a = 5;
+//    int b= 4;
+    int i = 0;
+    while (i <= b) {
+        i++;
+        a++;
+
+    }
+return a++;
+    }
+
+//        for (int i = 1; i < b; i++) {
+//            return a + a ;
+//        }
+//        return b;
+//    }
+}
