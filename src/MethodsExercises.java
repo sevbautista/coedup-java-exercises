344d97070cf26b5809c9fed5f1d108b10ed094da
Severiano Bautista <severiano.bautista090@gmail.com>
2023-12-14 10:49:36 -0600

    exercise so far

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
     public static void main(String[] args) {
 //        System.out.println(addition(5, 4));
@@ -6,54 +8,111 @@ public class MethodsExercises {
 //        System.out.println(division(5, 4));
 //        System.out.println(modulus(15, 5));
 //        System.out.println(byZero(1,0));
-        System.out.println(multiSansStar(5, 3));
+//        System.out.println(multiSansStar(5, 3));
+//        System.out.println(getInteger(1, 10));
+//        System.out.println(factorial(5));
     }
 
+//    1. BASIC ARITHMETIC
+//    1a1. ADDITION
 //    public static int addition(int a, int b) {
 //        return a + b;
 //
 //    }
-//
+
+//    1a2. SUBTRACTION
 //    public static int subtraction(int a, int b) {
 //        return a - b;
 //
 //    }
-//
+
+    //    1a3. MULTIPLICATION
 //    public static int multiplication(int a, int b) {
 //        return a * b;
 //
 //    }
-//
+
+    //    1a4. DIVISION
 //    public static int division(int a, int b) {
 //        return a / b;
 //
 //    }
-//
+
+//    1d. MODULUS
 //    public static int modulus(int a, int b) {
 //        return a % b;
 //
 //    }
 
-    //        public static int byZero (int a, int b) { // SYSTEM DOES NOT LIKE THIS
+//    DIVIDE BY ZERO
+//            public static int byZero (int a, int b) { // SYSTEM DOES NOT LIKE THIS
 //        return a / b; // SHOULD RETURN ZERO
 //
 //    }
-//     5 * 4
-    public static int multiSansStar(int a, int b) {
-//    int a = 5;
-//    int b= 4;
-    int i = 0;
-    while (i <= b) {
-        i++;
-        a++;
 
-    }
-return a++;
-    }
+
+//    MULTIPLICATION WITHOUT *
+//     5 * 4
+//    public static int multiSansStar(int a, int b) {
+////    int a = 5;
+////    int b= 4;
+//    int i = 0;
+//    while (i < b) {
+//        i++;
+//        ++a;
+//
+//    }
+//return a;
+//    }
 
 //        for (int i = 1; i < b; i++) {
 //            return a + a ;
 //        }
 //        return b;
 //    }
+
+//    public static int multiSansStar(int a, int b) {
+//        for (int i = 1; i < b; i++) {
+//            a += a;
+//        }
+//        return a;
+//    }
+
+//    public static int getInteger(int min, int max) {
+//        System.out.println("Enter a number between 1 and 10: ");
+//        Scanner scanner = new Scanner(System.in);
+//        int uI = scanner.nextInt();
+//
+//        if (uI <= max && uI >= min) {
+//            System.out.println("You are valid, you have worth.");
+//
+//        } else {
+//            System.out.println("try again, seek validation.");
+//            getInteger(min, max);
+//
+//        }
+//        return uI;
+//    }
+
+//    public static int factorial(int a) {
+//        System.out.println("Enter a number between 1 and 10: ");
+//        Scanner scanner = new Scanner(System.in);
+//        int uI = scanner.nextInt();
+//        int i, fact = 1;
+//        for (i = 1; i <= uI; i++) {
+//            fact = fact * i;
+//        }
+//        System.out.println(fact);
+//        return fact;
+//    }
+
+//            if (uI >= b) {
+//                int result = uI * (b - 1);
+//                return result;
+//
+//            }
+//            USER PUTS IN A NUMBER, THAT NUMBER WILL BE ITERATED DOWN TO
+//            return a;
+
+
 }
